f6fa7c3d01a304e81b57db427ff0e1ae9a2ba659
frankeeef <119160317+frankeeef@users.noreply.github.com>
2023-01-29 14:48:28 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<center>
+<b><h1>Ha <span style="color: red">Ha</span>.</h1></b>
+
+<img
+  src="https://media.tenor.com/SSY2V0RrU3IAAAAd/rick-roll-rick-rolled.gif"
+/>
+<audio controls autoplay>
+<source src="Never Gonna Give You Up Original.mp3" type="audio/mpeg" />
+<br>
+<br>
+</audio>
+
+`;
